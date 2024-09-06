113ef210d14bac80e0b0908aeeea2dde24a659c1
Lambdair <lambdair1984@protonmail.com>
2024-09-06 17:18:29 +0900

    fix: fix import error

--- Main.lean
+++ Main.lean
@@ -1,4 +1,3 @@
-import ComputationalTypeTheoryLean4
 
 def main : IO Unit :=
   IO.println s!"Hello, Lean!"
--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,12 @@
 import Lake
 open Lake DSL
 
-package "computational-type-theory-lean4" where
+package «computational-type-theory-lean4» where
   -- add package configuration options here
 
 lean_lib «ComputationalTypeTheoryLean4» where
   -- add library configuration options here
 
 @[default_target]
-lean_exe "computational-type-theory-lean4" where
+lean_exe «computational-type-theory-lean4» where
   root := `Main
